036a97e84bae8b74d7ebcd3314c06b1123c1bf1a
Seyerte <estherseyer@gmail.com>
2023-04-02 17:14:24 +0200

    first commit

---
+++ src/main/java/com/accenturef5/KatasJava/KatasJavaApplication.java
@@ -0,0 +1,13 @@
+package com.accenturef5.KatasJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatasJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenturef5/KatasJava/KatasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenturef5.KatasJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
